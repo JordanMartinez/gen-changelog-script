6b7f1123a6c67dc1dcb1b661419393b9564601b9
Jordan Martinez <jordanalex.martinez@gmail.com>
2021-01-01 00:09:01 -0800

    Port purescript-contrib-governance changelog generator to this repo

--- spago.dhall
+++ spago.dhall
@@ -3,7 +3,17 @@ Welcome to a Spago project!
 You can edit this file as you like.
 -}
 { name = "my-project"
-, dependencies = [ "console", "effect", "psci-support" ]
+, dependencies =
+  [ "aff"
+  , "affjax"
+  , "codec-argonaut"
+  , "console"
+  , "effect"
+  , "node-fs-aff"
+  , "optparse"
+  , "psci-support"
+  , "stringutils"
+  ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Main.purs
+++ src/Main.purs
@@ -3,8 +3,59 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Console (log)
+import Data.Foldable (fold)
+import Options.Applicative as OA
+import Data.String.Common (joinWith)
+import Effect.Aff (launchAff_)
+import Node.Encoding (Encoding(..))
+import Node.FS.Aff as FS
+import Changelog (generateChangelogContent)
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  args <- OA.execParser $ OA.info (OA.helper <*> parser) $ fold []
+  launchAff_ do
+    mainContent <- generateChangelogContent args
+    FS.writeTextFile UTF8 args.outputFile (initialChangelogFileText <> mainContent)
+
+parser :: OA.Parser { owner :: String, repo :: String, outputFile :: String }
+parser = ado
+  owner <- OA.strOption $ fold
+    [ OA.long "user"
+    , OA.short 'u'
+    , OA.metavar "USER"
+    , OA.help "The name of the user/organization who owns the repo on GitHub."
+    ]
+
+  repo <- OA.strOption $ fold
+    [ OA.long "repo"
+    , OA.short 'r'
+    , OA.metavar "REPO"
+    , OA.help "The name of the repository on GitHub, excluding the `purescript-` prefix. EX: `prelude`"
+    ]
+
+  outputFile <- OA.strOption $ fold
+    [ OA.long "output-file"
+    , OA.short 'o'
+    , OA.metavar "FILE"
+    , OA.help "The file that will contain the generated changelog file"
+    ]
+  in { owner, repo: "purescript-" <> repo, outputFile }
+
+initialChangelogFileText :: String
+initialChangelogFileText = joinWith "\n"
+  [ "# Changelog"
+  , ""
+  , "Notable changes to this project are documented in this file. The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html)."
+  , ""
+  , "## [Unreleased]"
+  , ""
+  , "Breaking changes (😱!!!):"
+  , ""
+  , "New features:"
+  , ""
+  , "Bugfixes:"
+  , ""
+  , "Other improvements:"
+  , ""
+  ]
