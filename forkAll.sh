1cfe6a0993fe8ecb6b674c4d0eebeffec8db5da0
Jordan Martinez <jordanalex.martinez@gmail.com>
2021-01-11 18:45:21 -0800

    Add web libraries

--- forkAll.sh
+++ forkAll.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-filename='purescript.txt'
+filename='purescript-web.txt'
 echo Start
 while read -r -u9 p; do
   pushd ../fourteen
-  gh repo fork purescript/purescript-$p --clone=true --remote=true
+  gh repo fork purescript-web/purescript-$p --clone=true --remote=true
   popd
 done 9< $filename
