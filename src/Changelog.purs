f3e2306eb88a889882076a4966c119ad31afc172
Jordan Martinez <jordanalex.martinez@gmail.com>
2021-05-29 13:15:36 -0700

    Remove required token CLI arg; account for possible null 'body' in codec

--- src/Changelog.purs
+++ src/Changelog.purs
@@ -3,9 +3,9 @@ module Changelog where
 import Prelude
 
 import Affjax as AX
-import Affjax.StatusCode as AXSC
 import Affjax.RequestHeader as AXRH
 import Affjax.ResponseFormat as RF
+import Affjax.StatusCode as AXSC
 import Data.Array (filter, null)
 import Data.Codec (decode)
 import Data.Codec.Argonaut (JsonCodec, array, printJsonDecodeError)
@@ -13,18 +13,20 @@ import Data.Codec.Argonaut as CA
 import Data.Codec.Argonaut.Record as CAR
 import Data.Either (Either(..))
 import Data.Foldable (foldl)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Data.Monoid (power)
 import Data.String (Pattern(..))
 import Data.String.CodeUnits (drop, indexOf, length, takeWhile)
 import Data.String.Common (joinWith, trim)
 import Data.String.Utils (lines)
 import Effect.Aff (Aff, error, throwError)
+import Effect.Class.Console (log)
+import Data.Codec.Argonaut.Compat as Compat
 
 type ReleaseInfo =
   { tag_name :: String
   , html_url :: String
-  , body :: String
+  , body :: Maybe String
   , published_at :: String
   , draft :: Boolean
   }
@@ -34,7 +36,7 @@ releaseCodec =
   CAR.object "ReleaseInfo" $
     { tag_name: CA.string
     , html_url: CA.string
-    , body: CA.string
+    , body: Compat.maybe CA.string
     , published_at: CA.string
     , draft: CA.boolean
     }
@@ -42,7 +44,7 @@ releaseCodec =
 type GhArgs r =
   { owner :: String
   , repo :: String
-  , token :: String
+  , token :: Maybe String
   | r
   }
 
@@ -61,7 +63,7 @@ generateChangelogContent gh = do
     addReleaseInfo acc rec =
       let
         dateWithoutTimeZone = takeWhile (_ /= 'T') rec.published_at
-        bodyWithFixedHeaders = fixHeaders $ trim rec.body
+        bodyWithFixedHeaders = fixHeaders $ trim $ fromMaybe "" rec.body
       in acc <> joinWith "\n"
         [ "## [" <> rec.tag_name <> "](" <> rec.html_url <> ") - " <> dateWithoutTimeZone
         , ""
@@ -92,34 +94,42 @@ generateChangelogContent gh = do
 
 recursivelyFetchReleases :: forall r. Array ReleaseInfo -> Int -> GhArgs r -> Aff (Array ReleaseInfo)
 recursivelyFetchReleases accumulator page gh = do
+  log $ "Fetching page #" <> show page
   pageNResult <- fetchNextPageOfReleases page gh
   case pageNResult of
-    Nothing -> pure accumulator
+    Nothing -> do
+      log $ "Page " <> show page <> " did not have any more releases"
+      pure accumulator
     Just arr -> recursivelyFetchReleases (accumulator <> arr) (page + 1) gh
 
 fetchNextPageOfReleases :: forall r. Int -> GhArgs r -> Aff (Maybe (Array ReleaseInfo))
 fetchNextPageOfReleases page gh = do
   let
     url = "https://api.github.com/repos/" <> gh.owner <> "/" <> gh.repo <> "/releases?per_page=100&page=" <> show page
-    authHeader = AXRH.RequestHeader "Authorization" $ "token" <> gh.token
     reqInfo = AX.defaultRequest
       { url = url
       , responseFormat = RF.json
-      , headers = [ authHeader ]
+      , headers = maybe mempty
+        (\t -> [ AXRH.RequestHeader "Authorization" $ "token" <> t ]) gh.token 
       }
 
   result <- AX.request reqInfo
   case result of
-    Right { body, status } | status == AXSC.StatusCode 200 ->
-      case decode (array releaseCodec) body of
-        Left e -> do
-          throwError $ error $ printJsonDecodeError e
-        Right releases | null releases ->
-          pure Nothing
-        Right releases -> do
-          pure $ Just releases
-    Right rec -> do
-      let errorMessage = show rec.status <> ": " <> show rec.statusText
-      throwError $ error errorMessage
+    Right rec
+      | rec.status == AXSC.StatusCode 200 ->
+          case decode (array releaseCodec) rec.body of
+            Left e -> do
+              throwError $ error $ printJsonDecodeError e
+            Right releases -> do
+              log $ show rec.status <> ": " <> show rec.statusText
+              if null releases then do
+                log "no releases found"
+                pure Nothing
+              else do
+                log "found releases"
+                pure $ Just releases
+      | otherwise -> do
+          let errorMessage = show rec.status <> ": " <> show rec.statusText
+          throwError $ error errorMessage
     Left err -> do
       throwError $ error $ AX.printError err
--- src/Main.purs
+++ src/Main.purs
@@ -6,6 +6,7 @@ import Effect (Effect)
 import Data.Foldable (fold)
 import Options.Applicative as OA
 import Data.String.Common (joinWith)
+import Data.Maybe (Maybe(..))
 import Effect.Aff (launchAff_)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff as FS
@@ -18,13 +19,14 @@ main = do
     mainContent <- generateChangelogContent args
     FS.writeTextFile UTF8 args.outputFile (initialChangelogFileText <> mainContent)
 
-parser :: OA.Parser { owner :: String, repo :: String, outputFile :: String, token :: String }
+parser :: OA.Parser { owner :: String, repo :: String, outputFile :: String, token :: Maybe String }
 parser = ado
-  token <- OA.strOption $ fold
+  t <- OA.strOption $ fold
     [ OA.long "token"
     , OA.short 't'
     , OA.metavar "GITHUB_TOKEN"
     , OA.help "The GitHub Auth token that prevents this program from being rate-limited"
+    , OA.value ""
     ]
   owner <- OA.strOption $ fold
     [ OA.long "user"
@@ -37,7 +39,7 @@ parser = ado
     [ OA.long "repo"
     , OA.short 'r'
     , OA.metavar "REPO"
-    , OA.help "The name of the repository on GitHub, excluding the `purescript-` prefix. EX: `prelude`"
+    , OA.help "The name of the repository on GitHub. EX: `purescript-prelude`"
     ]
 
   outputFile <- OA.strOption $ fold
@@ -46,7 +48,7 @@ parser = ado
     , OA.metavar "FILE"
     , OA.help "The file that will contain the generated changelog file"
     ]
-  in { owner, repo: "purescript-" <> repo, outputFile, token }
+  in { owner, repo, outputFile, token: if t == "" then Nothing else Just t }
 
 initialChangelogFileText :: String
 initialChangelogFileText = joinWith "\n"
